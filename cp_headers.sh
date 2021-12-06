d27086061327457d7f725dc4697917cd4da42314
Alin Jerpelea <alin.jerpelea@sony.com>
2021-12-06 13:23:44 +0000

    cp_headers: add initial script
    add initial script for headersetup
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

---
+++ cp_headers.sh
@@ -0,0 +1,123 @@
+CLEAN_HEADER=bionic/libc/kernel/tools/clean_header.py
+
+KERNEL_PATH=kernel/sony/msm-5.4
+HEADER_SRC=$KERNEL_PATH/kernel/include
+HEADER_ORI=$KERNEL_PATH/common-headers/original-kernel-headers
+HEADER_SAN=$KERNEL_PATH/common-headers/kernel-headers
+
+LINUX_HEADERS="\
+    linux/hdmi.h"
+
+UAPI_HEADERS="\
+    sound/compress_params.h\
+    sound/compress_offload.h\
+    sound/asound.h\
+    linux/ion.h\
+    linux/msm_ion.h \
+    linux/v4l2-mediabus.h\
+    linux/msm_rmnet.h\
+    linux/msm_ipa.h\
+    linux/videodev2.h\
+    linux/fb.h\
+    linux/fuse.h\
+    linux/ipa_qmi_service_v01.h\
+    linux/netlink.h\
+    linux/msm_kgsl.h\
+    linux/msm_dsps.h\
+    linux/rmnet_data.h\
+    linux/rmnet_ipa_fd_ioctl.h\
+    linux/socket.h\
+    linux/qseecom.h\
+    linux/spcom.h\
+    linux/v4l2-common.h\
+    linux/v4l2-controls.h\
+    linux/v4l2-mediabus.h\
+    linux/mmc/ioctl.h\
+    drm/msm_drm.h\
+    drm/drm_fourcc.h\
+    drm/drm.h \
+    drm/drm_mode.h \
+    asm-generic/ioctls.h"
+
+TECHPACK_AUDIO_UAPI_HEADERS="\
+    sound/audio_effects.h\
+    sound/lsm_params.h\
+    sound/devdep_params.h\
+    sound/msmcal-hwdep.h\
+    sound/voice_params.h\
+    sound/wcd-dsp-glink.h\
+    linux/mfd/wcd9xxx/wcd9xxx_registers.h\
+    linux/mfd/wcd9xxx/wcd9320_registers.h\
+    linux/msm_audio_calibration.h\
+    linux/msm_audio_wmapro.h\
+    linux/msm_audio_amrwb.h\
+    linux/msm_audio_amrwbplus.h\
+    linux/msm_audio_wma.h\
+    linux/msm_audio_aac.h\
+    linux/msm_audio.h"
+
+TECHPACK_CAMERA_UAPI_HEADERS="\
+    media/cam_cpas.h\
+    media/cam_custom.h\
+    media/cam_defs.h\
+    media/cam_fd.h\
+    media/cam_icp.h\
+    media/cam_isp.h\
+    media/cam_isp_ife.h\
+    media/cam_isp_tfe.h\
+    media/cam_isp_vfe.h\
+    media/cam_jpeg.h\
+    media/cam_lrme.h\
+    media/cam_ope.h\
+    media/cam_req_mgr.h\
+    media/cam_sensor.h\
+    media/cam_sync.h\
+    media/cam_tfe.h\
+    media/cam_defs.h\
+    media/cam_sensor.h\
+    media/cam_isp.h\
+    media/cam_isp_ife.h\
+    media/cam_isp_vfe.h\
+    media/cam_icp.h\
+    media/cam_cpas.h\
+    media/cam_fd.h\
+    media/cam_jpeg.h\
+    media/cam_lrme.h\
+    media/cam_req_mgr.h\
+    media/cam_sync.h"
+
+TECHPACK_DISPLAY_UAPI_HEADERS="\
+    drm/msm_drm_pp.h\
+    drm/sde_drm.h"
+
+cd ../../../..
+
+source build/envsetup.sh
+lunch 50
+
+for x in $LINUX_HEADERS; do \
+cp $HEADER_SRC/$x $HEADER_ORI/$x
+$CLEAN_HEADER -u -v -k $HEADER_ORI -d $HEADER_SAN $x &>out/cp_headers_5.4.log
+done
+
+for x in $UAPI_HEADERS; do \
+cp $HEADER_SRC/"uapi/"$x $HEADER_ORI/$x
+$CLEAN_HEADER -u -v -k $HEADER_ORI -d $HEADER_SAN $x &>>out/cp_headers_5.4.log
+done
+
+for x in $TECHPACK_AUDIO_UAPI_HEADERS; do \
+cp $HEADER_SRC/"../techpack/audio/include/uapi/audio/"$x $HEADER_ORI/$x
+$CLEAN_HEADER -u -v -k $HEADER_ORI -d $HEADER_SAN $x &>>out/cp_headers_5.4.log
+done
+
+for x in $TECHPACK_CAMERA_UAPI_HEADERS; do \
+cp $HEADER_SRC/"../techpack/camera/include/uapi/camera/"$x $HEADER_ORI/$x
+$CLEAN_HEADER -u -v -k $HEADER_ORI -d $HEADER_SAN $x &>>out/cp_headers_5.4.log
+done
+
+for x in $TECHPACK_DISPLAY_UAPI_HEADERS; do \
+cp $HEADER_SRC/"../techpack/display/include/uapi/display/"$x $HEADER_ORI/$x
+$CLEAN_HEADER -u -v -k $HEADER_ORI -d $HEADER_SAN $x &>>out/cp_headers_5.4.log
+done
+
+echo "Copy complete!"
---
+++ kernel-headers/media/cam_sensor.h
@@ -0,0 +1,218 @@
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ***   To edit the content of this header, modify the corresponding
+ ***   source file (e.g. under external/kernel-headers/original/) then
+ ***   run bionic/libc/kernel/tools/update_all.py
+ ***
+ ***   Any manual change here will be lost the next time this script will
+ ***   be run. You've been warned!
+ ***
+ ****************************************************************************
+ ****************************************************************************/
+#ifndef __UAPI_CAM_SENSOR_H__
+#define __UAPI_CAM_SENSOR_H__
+#include <linux/types.h>
+#include <linux/ioctl.h>
+#include <camera/media/cam_defs.h>
+#define CAM_SENSOR_PROBE_CMD (CAM_COMMON_OPCODE_MAX + 1)
+#define CAM_FLASH_MAX_LED_TRIGGERS 2
+#define MAX_OIS_NAME_SIZE 32
+#define CAM_CSIPHY_SECURE_MODE_ENABLED 1
+struct cam_sensor_query_cap {
+  __u32 slot_info;
+  __u32 secure_camera;
+  __u32 pos_pitch;
+  __u32 pos_roll;
+  __u32 pos_yaw;
+  __u32 actuator_slot_id;
+  __u32 eeprom_slot_id;
+  __u32 ois_slot_id;
+  __u32 flash_slot_id;
+  __u32 csiphy_slot_id;
+} __attribute__((packed));
+struct cam_csiphy_query_cap {
+  __u32 slot_info;
+  __u32 version;
+  __u32 clk_lane;
+  __u32 reserved;
+} __attribute__((packed));
+struct cam_actuator_query_cap {
+  __u32 slot_info;
+  __u32 reserved;
+} __attribute__((packed));
+struct cam_eeprom_query_cap_t {
+  __u32 slot_info;
+  __u16 eeprom_kernel_probe;
+  __u16 is_multimodule_mode;
+} __attribute__((packed));
+struct cam_ois_query_cap_t {
+  __u32 slot_info;
+  __u16 reserved;
+} __attribute__((packed));
+struct cam_cmd_i2c_info {
+  __u32 slave_addr;
+  __u8 i2c_freq_mode;
+  __u8 cmd_type;
+  __u16 reserved;
+} __attribute__((packed));
+struct cam_ois_opcode {
+  __u32 prog;
+  __u32 coeff;
+  __u32 pheripheral;
+  __u32 memory;
+} __attribute__((packed));
+struct cam_cmd_ois_info {
+  __u32 slave_addr;
+  __u8 i2c_freq_mode;
+  __u8 cmd_type;
+  __u8 ois_fw_flag;
+  __u8 is_ois_calib;
+  char ois_name[MAX_OIS_NAME_SIZE];
+  struct cam_ois_opcode opcode;
+} __attribute__((packed));
+struct cam_cmd_probe {
+  __u8 data_type;
+  __u8 addr_type;
+  __u8 op_code;
+  __u8 cmd_type;
+  __u32 reg_addr;
+  __u32 expected_data;
+  __u32 data_mask;
+  __u16 camera_id;
+  __u16 reserved;
+} __attribute__((packed));
+struct cam_power_settings {
+  __u16 power_seq_type;
+  __u16 reserved;
+  __u32 config_val_low;
+  __u32 config_val_high;
+} __attribute__((packed));
+struct cam_cmd_power {
+  __u32 count;
+  __u8 reserved;
+  __u8 cmd_type;
+  __u16 more_reserved;
+  struct cam_power_settings power_settings[1];
+} __attribute__((packed));
+struct i2c_rdwr_header {
+  __u32 count;
+  __u8 op_code;
+  __u8 cmd_type;
+  __u8 data_type;
+  __u8 addr_type;
+} __attribute__((packed));
+struct i2c_random_wr_payload {
+  __u32 reg_addr;
+  __u32 reg_data;
+} __attribute__((packed));
+struct cam_cmd_i2c_random_wr {
+  struct i2c_rdwr_header header;
+  struct i2c_random_wr_payload random_wr_payload[1];
+} __attribute__((packed));
+struct cam_cmd_read {
+  __u32 reg_data;
+  __u32 reserved;
+} __attribute__((packed));
+struct cam_cmd_i2c_continuous_wr {
+  struct i2c_rdwr_header header;
+  __u32 reg_addr;
+  struct cam_cmd_read data_read[1];
+} __attribute__((packed));
+struct cam_cmd_i2c_random_rd {
+  struct i2c_rdwr_header header;
+  struct cam_cmd_read data_read[1];
+} __attribute__((packed));
+struct cam_cmd_i2c_continuous_rd {
+  struct i2c_rdwr_header header;
+  __u32 reg_addr;
+} __attribute__((packed));
+struct cam_cmd_conditional_wait {
+  __u8 data_type;
+  __u8 addr_type;
+  __u16 reserved;
+  __u8 op_code;
+  __u8 cmd_type;
+  __u16 timeout;
+  __u32 reg_addr;
+  __u32 reg_data;
+  __u32 data_mask;
+} __attribute__((packed));
+struct cam_cmd_unconditional_wait {
+  __s16 delay;
+  __s16 reserved;
+  __u8 op_code;
+  __u8 cmd_type;
+  __u16 reserved1;
+} __attribute__((packed));
+struct cam_csiphy_info {
+  __u16 reserved;
+  __u16 lane_assign;
+  __u16 mipi_flags;
+  __u8 lane_cnt;
+  __u8 secure_mode;
+  __u64 settle_time;
+  __u64 data_rate;
+} __attribute__((packed));
+struct cam_csiphy_acquire_dev_info {
+  __u32 combo_mode;
+  __u16 cphy_dphy_combo_mode;
+  __u8 csiphy_3phase;
+  __u8 reserve;
+} __attribute__((packed));
+struct cam_sensor_acquire_dev {
+  __u32 session_handle;
+  __u32 device_handle;
+  __u32 handle_type;
+  __u32 reserved;
+  __u64 info_handle;
+} __attribute__((packed));
+struct cam_sensor_streamon_dev {
+  __u32 session_handle;
+  __u32 device_handle;
+  __u32 handle_type;
+  __u32 reserved;
+  __u64 info_handle;
+} __attribute__((packed));
+struct cam_flash_init {
+  __u32 flash_type;
+  __u8 reserved;
+  __u8 cmd_type;
+  __u16 reserved1;
+} __attribute__((packed));
+struct cam_flash_set_rer {
+  __u32 count;
+  __u8 opcode;
+  __u8 cmd_type;
+  __u16 num_iteration;
+  __u32 led_on_delay_ms;
+  __u32 led_off_delay_ms;
+  __u32 led_current_ma[CAM_FLASH_MAX_LED_TRIGGERS];
+} __attribute__((packed));
+struct cam_flash_set_on_off {
+  __u32 count;
+  __u8 opcode;
+  __u8 cmd_type;
+  __u16 reserved;
+  __u32 led_current_ma[CAM_FLASH_MAX_LED_TRIGGERS];
+  __u64 time_on_duration_ns;
+} __attribute__((packed));
+struct cam_flash_query_curr {
+  __u16 reserved;
+  __u8 opcode;
+  __u8 cmd_type;
+  __u32 query_current_ma;
+} __attribute__((packed));
+struct cam_flash_query_cap_info {
+  __u32 slot_info;
+  __u32 max_current_flash[CAM_FLASH_MAX_LED_TRIGGERS];
+  __u32 max_duration_flash[CAM_FLASH_MAX_LED_TRIGGERS];
+  __u32 max_current_torch[CAM_FLASH_MAX_LED_TRIGGERS];
+} __attribute__((packed));
+#endif
